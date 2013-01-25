16b416e35a0587cd6fb5cc98289a3101164870c3
Connor Tumbleson <connor.tumbleson@gmail.com>
2013-01-24 23:14:52 -0600

    fixing structs, bed

--- application/libraries/structs/Category_Id.php
+++ application/libraries/structs/Category_Id.php
@@ -3,9 +3,11 @@
 /**
  * Our enum of Category IDs.
  */
-final class CategoryID {
+final class Category_Id{
 
     const CAMPAIGN = 0x00;
     const SPARTAN_OPS = 0x01;
     const WAR_GAMES = 0x02;
+    const WAYPOINT = 0x03;
+    
 }
--- application/libraries/structs/Period_Id.php
+++ application/libraries/structs/Period_Id.php
@@ -3,7 +3,7 @@
 /**
  * Our enum of Period IDs.
  */
-final class PeriodID {
+final class Period_Id{
 
     const DAILY = 0x00;
     const WEEKLY = 0x01;
