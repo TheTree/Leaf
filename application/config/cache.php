b113b864a1d33619832a188a18169c4eedf702e0
Connor Tumbleson <connor.tumbleson@gmail.com>
2013-01-24 20:31:40 -0600

    adding Phil stuff

---
+++ application/config/cache.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Directory where CACHE should stored.
+ * @todo Store off of public_html
+ */
+$config['cache_dir'] = APPPATH.'cache/';
+$config['cache_default_expires'] = 0;
+
+?>
