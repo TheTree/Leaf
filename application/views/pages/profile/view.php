c8c78d642157e3b05cdae368267fc2415706f0ca
Connor Tumbleson <connor.tumbleson@gmail.com>
2013-01-27 22:19:16 -0600

    added check if finished current spec, but haven't moved on

--- application/views/pages/profile/view.php
+++ application/views/pages/profile/view.php
@@ -12,9 +12,17 @@
 
     <div class="span9">
         <br /><br />
-        <?= number_format($data['NextRankStartXP'] - $data['Xp']); ?> XP till next level (<?= round(floatval(($data['Xp'] - $data['RankStartXP'])  / ($data['NextRankStartXP'] - $data['RankStartXP'])) * 100); ?>% complete)
+            <? if ($data['NextRankStartXP'] == 0): ?>
+                Specialization: <?= $data['Specialization']; ?> Completed
+            <? else: ?>
+                <?= number_format($data['NextRankStartXP'] - $data['Xp']); ?> XP till next level (<?= round(floatval(($data['Xp'] - $data['RankStartXP'])  / ($data['NextRankStartXP'] - $data['RankStartXP'])) * 100); ?>% complete)
+            <? endif; ?>
         <div class="progress progress-striped">
-            <div class="bar" style="width: <?= floatval(($data['Xp'] - $data['RankStartXP'])  / ($data['NextRankStartXP'] - $data['RankStartXP'])) * 100; ?>%;"></div>
+            <? if ($data['NextRankStartXP'] == 0): ?>
+                <div class="bar" style="width: 100%;"></div>
+            <? else: ?>
+                <div class="bar" style="width: <?= floatval(($data['Xp'] - $data['RankStartXP'])  / ($data['NextRankStartXP'] - $data['RankStartXP'])) * 100; ?>%;"></div>
+            <? endif; ?>
         </div>
         <strong>Basic Stats</strong>
         <div class="well well-large">
