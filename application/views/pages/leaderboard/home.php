829f9f1cd693155457b28530683aa6dd13b081d9
Connor Tumbleson <connor.tumbleson@gmail.com>
2013-02-05 19:56:44 -0600

    experimenting with a cool leaderboard design

--- application/views/pages/leaderboard/home.php
+++ application/views/pages/leaderboard/home.php
@@ -1,1 +1,28 @@
+<div class="row">
+    <? if ($stats['TotalKills'] != false): ?>
+        <div class="span3">
+            <strong>Total Kills</strong><br />
+            <div class="well">
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Gamertag</th>
+                            <th>Kills</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <? foreach ($stats['TotalKills'] as $item): ?>
+                            <tr>
+                                <td></td>
+                                <td><a href="<?= base_url('gt/' . str_replace(" ", "_", $item['Gamertag'])); ?>"><?= $item['Gamertag']; ?></a></td>
+                                <td><?= number_format($item['TotalKills']); ?></td>
+                            </tr>
+                        <? endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    <? endif; ?>
+</div>
 <?= output($stats); ?>
