443488a270bac047735045d3d4395744c5b5e388
Connor Tumbleson <connor.tumbleson@gmail.com>
2013-05-11 18:47:01 -0500

    pushed live to leafapp

--- application/views/_partials/profile/block_csr.php
+++ application/views/_partials/profile/block_csr.php
@@ -23,7 +23,7 @@
                     <tbody>
                         <? foreach($data['CSRPlaylist'] as $playlist): ?>
                             <tr>
-                                <td><?= $playlist['Name']; ?></td>
+                                <td><?= $playlist['Name']; ?><a href="<?= base_url('csr_leaderboards/' . $playlist['Id']); ?>"><i class="icon-leaf"></i></a></td>
                                 <td><span class="flair flair-CSR-<?= $playlist['SkillRank']; ?>"></span></td>
                                 <td><?= $this->library->get_trophy($playlist['Rank']); ?></td>
                             </tr>
