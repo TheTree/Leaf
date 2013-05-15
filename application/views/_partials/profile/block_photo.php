56ee2b1766d77724e7f857cd1f51cb7850d3843a
Connor Tumbleson <connor.tumbleson@gmail.com>
2013-05-15 06:14:51 -0500

    fixed #17

--- application/views/_partials/profile/block_photo.php
+++ application/views/_partials/profile/block_photo.php
@@ -1,3 +1,13 @@
+<? if (($data['Expiration'] - TWENTYFOUR_HOURS_IN_SECONDS - time() + FIVEMIN_IN_SECONDS) > 0): ?>
+<script type="text/javascript">
+    function show_recache() {
+        $('#recache_button').show();
+    }
+    $(document).ready(function() {
+        setTimeout(show_recache, <?= (($data['Expiration'] - TWENTYFOUR_HOURS_IN_SECONDS - time() + FIVEMIN_IN_SECONDS) * 1000) ?>);
+    });
+</script>
+<? endif; ?>
 <img src="<?= $data['SpartanURL']; ?>" width="211px" height="418px" class="img-polaroid" />
 <div class="well well-large btn btn-primary pad10 w150">
     <div class="pagination-centered">
@@ -6,9 +16,10 @@
     </div>
 </div>
 <br />
-<!-- setTimeout(function(){$('img').fadeOut(2000);}, 5000); -->
 <? if ((($data['Expiration'] - TWENTYFOUR_HOURS_IN_SECONDS + FIVEMIN_IN_SECONDS) < time()) && $data['InactiveCounter'] < 40): ?>
     <a href="<?= base_url('gt/' . $data['SeoGamertag'] . "/recache"); ?>" class="btn btn-success pad10">Refresh</a>
+<? elseif (($data['Expiration'] - TWENTYFOUR_HOURS_IN_SECONDS - time() + FIVEMIN_IN_SECONDS) > 0): ?>
+    <a id="recache_button" href="<?= base_url('gt/' . $data['SeoGamertag'] . "/recache"); ?>" style="display:none;" class="btn btn-success pad10">Refresh</a>
 <? endif; ?>
 <? if ($data['InactiveCounter'] >= 40): ?>
     <a href="<?= base_url('unfreeze/' . $data['SeoGamertag']); ?>" class="btn btn-success margin3pxtop">Unfreeze</a>
