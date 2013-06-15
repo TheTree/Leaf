b8f87753a93441a44b496d1e2a222f85e47d00a3
Connor Tumbleson <connor.tumbleson@gmail.com>
2013-06-14 22:30:04 -0500

    remove "Castle DLC / Forge Playlist" due to 343's stupid fault of deleting old playlists data from API even though thousands of people spend hours on hours on days getting a rank there. Just for it to get deleted with no trace :/

--- application/config/h4_leaf.php
+++ application/config/h4_leaf.php
@@ -5,12 +5,12 @@
  *
  * via PlaylistID
  */
-$config['individual_csr'] = [101,104,100,116,103,128,115,126,113,131];
+$config['individual_csr'] = [101,104,100,116,103,128,115,126,113];
 
 /**
  * TEAM CSR
  */
-$config['team_csr'] = [105,129,102,117,121,122,123,124];
+$config['team_csr'] = [105,102,117,121,122,123,124];
 
 /**
  * 343 Easter Eggs
