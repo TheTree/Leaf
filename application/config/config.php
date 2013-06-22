78e2d2574041db676a4c10d8102b9b24743414be
Connor Tumbleson <connor.tumbleson@gmail.com>
2013-06-22 14:33:08 -0500

    drop threshold to 1

--- application/config/config.php
+++ application/config/config.php
@@ -205,7 +205,7 @@ $config['directory_trigger']	= 'd'; // experimental not currently in use
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 4;
+$config['log_threshold'] = 1;
 
 /*
 |--------------------------------------------------------------------------
