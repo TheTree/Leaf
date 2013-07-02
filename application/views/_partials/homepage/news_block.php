ca6201a4918ac403973ddd2907a741444a3cc668
Connor Tumbleson <connor.tumbleson@gmail.com>
2013-07-02 14:38:37 -0500

    add autocomplete to homepage

--- application/views/_partials/homepage/news_block.php
+++ application/views/_partials/homepage/news_block.php
@@ -8,9 +8,20 @@
 <div class="well well-large">
     <?= form_open('home/index', array('class' => 'form-search')); ?>
     <div class="control-group <? if (form_error('gamertag') != null): ?>error<? endif; ?>">
-        <input type="text" class="input-xlarge" name="gamertag" id="gamertag" placeholder="Enter your gamertag">
+        <input type="text" class="input-xlarge ajax-typeahead" autocomplete="off" name="gamertag" id="gamertag" placeholder="Enter your gamertag">
         <button type="submit" class="btn btn-primary">Load Stats</button>
         <? if (form_error('gamertag') != null): ?><br /><span class="help-inline"><?= form_error('gamertag') ?></span><? endif; ?>
     </div>
     <?= form_close(); ?>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#gamertag').typeahead({
+                source: function(query, process) {
+                    return $.post('<?= base_url('ajax/gt/'); ?>' + "/" + query, {}, function(data) {
+                        return process(data);
+                    });
+                }
+            });
+        });
+    </script>
 </div>
