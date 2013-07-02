42c08ad7b88493c3fc3835b68b5cb4e388728f7c
Connor Tumbleson <connor.tumbleson@gmail.com>
2013-07-02 07:46:49 -0500

    was wrong :( (Big Team Skirmish is Individual CSR)

--- application/config/h4_leaf.php
+++ application/config/h4_leaf.php
@@ -5,12 +5,12 @@
  *
  * via PlaylistID
  */
-$config['individual_csr'] = [101,104,100,116,103,128,115,126,113];
+$config['individual_csr'] = [101,104,100,116,103,128,115,126,113,134];
 
 /**
  * TEAM CSR
  */
-$config['team_csr'] = [105,102,117,121,122,123,124,134];
+$config['team_csr'] = [105,102,117,121,122,123,124];
 
 /**
  * 343 Easter Eggs
