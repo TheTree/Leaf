59a19a7b75869614b6b1cf9a05295dd62b9e66dc
Connor Tumbleson <connor.tumbleson@gmail.com>
2013-07-07 09:36:47 -0500

    remove team objective (gone from api)

--- application/config/h4_leaf.php
+++ application/config/h4_leaf.php
@@ -9,8 +9,9 @@ $config['individual_csr'] = [101,104,100,116,103,128,115,126,113,134];
 
 /**
  * TEAM CSR
+ * 122 - team objective (removed)
  */
-$config['team_csr'] = [105,102,117,121,122,123,124];
+$config['team_csr'] = [105,102,117,121,123,124];
 
 /**
  * 343 Easter Eggs
