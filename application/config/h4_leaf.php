100254f292debdd8f642022ce4e9da2514f224a8
Connor Tumbleson <connor.tumbleson@gmail.com>
2013-07-17 18:53:33 -0500

    remove old code, update list

--- application/config/h4_leaf.php
+++ application/config/h4_leaf.php
@@ -1,22 +1,35 @@
 <?php
 
 /**
+ * 95           - Global Challenge
+ * 100          - Infinity Slayer
+ * 101          - Big Team Infinity Slayer
+ * 102          - Dominion
+ * 103          - Regicide
+ * 104          - Flood
+ * 105          - Capture The Flag
+ * 113          - Team Snipers
+ * 115          - SWAT
+ * 116          - Multi-Team
+ * 117          - Griffball
+ * 119          - Crimson DLC
+ * 121          - Team Doubles
+ * 123          - Team Throwdown
+ * 124          - Majestic Team DLC
+ * 126          - Team Action Sack
+ * 128          - Rumble Pit
+ * 134          - Big Team Skirmish
+ * 135          - Legendary Slayer
+ */
+
+
+/**
  * INDIVIDUAL CSR
- *
- * via PlaylistID
- * 95 - Global Challenge
- * 135 - Legendary Slayer
  */
 $config['individual_csr'] = [101,104,100,116,103,128,115,126,113,134,135,95];
 
 /**
  * TEAM CSR
- * 122 - team objective
- * 124 - Majestic Team
  */
 $config['team_csr'] = [105,102,117,121,123,119];
 
-/**
- * 343 Easter Eggs
- */
-$config['employees_343'] = ["bs angel"];
