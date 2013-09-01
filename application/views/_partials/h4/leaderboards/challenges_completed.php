8af2b5c401664ca3d2aa5b18ea1c37c800bf7014
Connor Tumbleson <connor.tumbleson@gmail.com>
2013-09-01 13:27:35 -0500

    fix top ten to bootstrap3

--- application/views/_partials/h4/leaderboards/challenges_completed.php
+++ application/views/_partials/h4/leaderboards/challenges_completed.php
@@ -1,27 +1,25 @@
 <? if ($stats['Data']['challenges_completed'] != false): ?>
-    <div class="well">
-        <table class="table table-bordered table-striped">
-            <thead>
+    <table class="table table-bordered table-striped">
+        <thead>
+        <tr>
+            <th></th>
+            <th>Gamertag</th>
+            <th>Challenges</th>
+        </tr>
+        </thead>
+        <tbody>
+        <? $x = 1; foreach ($stats['Data']['challenges_completed'] as $item): ?>
             <tr>
-                <th></th>
-                <th>Gamertag</th>
-                <th>Challenges</th>
+                <td><?= $this->h4_lib->get_trophy($x); ?></td>
+                <td><a href="<?= base_url('h4/record/' . $item[H4::SEO_GAMERTAG]); ?>">
+                        <?= $item[H4::GAMERTAG]; ?>
+                        <? if (isset($item[H4::BADGE])): ?>
+                            - <span class="badge badge-<?= $item[H4::BADGE_COLOR]; ?>"><?= $item[H4::BADGE]; ?></span>
+                        <? endif; ?>
+                    </a></td>
+                <td><?= number_format($item[H4::TOTAL_CHALLENGES_COMPLETED]); ?></td>
             </tr>
-            </thead>
-            <tbody>
-            <? $x = 1; foreach ($stats['Data']['challenges_completed'] as $item): ?>
-                <tr>
-                    <td><?= $this->h4_lib->get_trophy($x); ?></td>
-                    <td><a href="<?= base_url('h4/record/' . $item[H4::SEO_GAMERTAG]); ?>">
-                            <?= $item[H4::GAMERTAG]; ?>
-                            <? if (isset($item[H4::BADGE])): ?>
-                                - <span class="badge badge-<?= $item[H4::BADGE_COLOR]; ?>"><?= $item[H4::BADGE]; ?></span>
-                            <? endif; ?>
-                        </a></td>
-                    <td><?= number_format($item[H4::TOTAL_CHALLENGES_COMPLETED]); ?></td>
-                </tr>
-                <? $x++; endforeach; ?>
-            </tbody>
-        </table>
-    </div>
+            <? $x++; endforeach; ?>
+        </tbody>
+    </table>
 <? endif; ?>
--- application/views/pages/h4/leaderboard/top_ten.php
+++ application/views/pages/h4/leaderboard/top_ten.php
@@ -1,25 +1,20 @@
-<br /><br /><br />
-<header class="jumbotron subhead" id="overview">
-<div class="subnav">
-    <ul class="nav nav-pills">
-        <? foreach($stats['Items'] as $item): ?>
-            <li><a href="#<?= strtolower(str_replace(" ", "_",$item)); ?>"><?= $item; ?></a></li>
-        <? endforeach; ?>
-        <li><a</li>
-    </ul>
+<br />
+<div class="subnav text-center">
+    <? foreach($stats['Items'] as $item): ?>
+        <a class="label label-static-size label-primary" href="#<?= strtolower(str_replace(" ", "_",$item)); ?>"><?= $item; ?></a>
+    <? endforeach; ?>
 </div>
-</header>
-<div class="row-fluid"
-<? foreach($stats['Items'] as $item): ?>
-    <section id="<?= strtolower(str_replace(" ", "_",$item)); ?>">
-        <div class="page-header"><h1><?= $item; ?></h1></div>
-        <div class="fieldset-content">
-            <p>
-                <?= $template['_partials'][strtolower(str_replace(" ", "_",$item))]; ?>
-            </p>
-        </div>
-    </section>
-<? endforeach; ?>
+<div class="row">
+    <? foreach($stats['Items'] as $item): ?>
+        <section id="<?= strtolower(str_replace(" ", "_",$item)); ?>">
+            <div class="page-header"><h1><?= $item; ?></h1></div>
+            <div class="fieldset-content">
+                <p>
+                    <?= $template['_partials'][strtolower(str_replace(" ", "_",$item))]; ?>
+                </p>
+            </div>
+        </section>
+    <? endforeach; ?>
 </div>
 <script type="text/javascript">
     $(document).ready(function() {
