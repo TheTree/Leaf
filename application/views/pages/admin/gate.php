77958b0ac8f61a832aaa5c860438e1f096862235
Connor Tumbleson <connor.tumbleson@gmail.com>
2013-09-07 11:05:09 -0500

    [wip] backstage boot3 tweaking

--- application/views/pages/admin/gate.php
+++ application/views/pages/admin/gate.php
@@ -1,27 +1,25 @@
-<div class="row-fluid">
-    <div class="span6 offset3">
-        <?= form_open('backstage', array('class' => 'form-horizontal')); ?>
-        <fieldset>
+<div class="row">
+    <div class="col-md-offset-3 col-md-6">
+        <?= form_open('backstage', array('class' => 'form-horizontal', 'role' => 'form')); ?>
             <legend>Welcome to the Sandbox</legend>
-            <div class="control-group <? if (form_error('username') != null): ?>error<? endif; ?>">
+            <div class="form-group <? if (form_error('username') != null): ?>error<? endif; ?>">
                 <label class="control-label" for="author">Username </label>
-                <div class="controls">
-                    <input type="text" name="username" id="username" value="<?= set_value('username'); ?>">
+                <div class="col-lg-12">
+                    <input class="form-control" type="text" name="username" id="username" value="<?= set_value('username'); ?>">
                     <? if (form_error('username') != null): ?><span class="help-inline"><?= form_error('username') ?></span><? endif; ?>
                 </div>
             </div>
-            <div class="control-group <? if (form_error('password') != null): ?>error<? endif; ?>">
+            <div class="form-group <? if (form_error('password') != null): ?>error<? endif; ?>">
                 <label class="control-label" for="pass">Password</label>
-                <div class="controls">
-                    <input type="password" name="password" id="password">
+                <div class="col-lg-12">
+                    <input type="password" class="form-control" name="password" id="password">
                     <? if (form_error('password') != null): ?><span class="help-inline"><?= form_error('password') ?></span><? endif; ?>
                 </div>
             </div>
-            <div class="form-actions">
+            <div class="form-group">
                 <button type="submit" class="btn">Sign In</button>
             </div>
             <span class="badge badge-inverse">Pikmin 3 ♥</span>
-        </fieldset>
         <?= form_close(); ?>
     </div>
 </div>
