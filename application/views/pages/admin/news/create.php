ee5fc6d49ceae4a19f7af90811a95e3e6cc7476a
Connor Tumbleson <connor.tumbleson@gmail.com>
2013-10-19 14:44:36 -0500

    bootstrap acp/news/add

--- application/views/pages/admin/news/create.php
+++ application/views/pages/admin/news/create.php
@@ -1,25 +1,25 @@
-<?= form_open('backstage/news/create', array('class' => 'form-horizontal')); ?>
+<?= form_open('backstage/news/create', array('class' => 'form-vertical', 'role' => 'form')); ?>
 <?= form_hidden('submitted', TRUE); ?>
     <fieldset>
         <legend>Create News Story</legend>
-        <div class="control-group <? if (form_error('title') != null): ?>error<? endif; ?>">
-            <label class="control-label" for="title">Title </label>
+        <div class="form-group <? if (form_error('title') != null): ?>error<? endif; ?>">
+            <label for="title">Title</label>
             <div class="controls">
-                <input class="input-xlarge" type="text" name="title" id="title" value="<?= set_value('title'); ?>">
+                <input class="form-control input-xlarge" type="text" name="title" id="title" value="<?= set_value('title'); ?>">
                 <? if (form_error('title') != null): ?><span class="help-inline"><?= form_error('title') ?></span><? endif; ?>
             </div>
         </div>
-        <div class="control-group <? if (form_error('author') != null): ?>error<? endif; ?>">
-            <label class="control-label" for="author">Author </label>
+        <div class="form-group <? if (form_error('author') != null): ?>error<? endif; ?>">
+            <label for="author">Author</label>
             <div class="controls">
-                <input class="input-xlarge" type="text" name="author" id="author" value="<?= set_value('author'); ?>">
+                <input class="form-control input-xlarge" type="text" name="author" id="author" value="<?= set_value('author'); ?>">
                 <? if (form_error('author') != null): ?><span class="help-inline"><?= form_error('author') ?></span><? endif; ?>
             </div>
         </div>
-        <div class="control-group <? if (form_error('article') != null): ?>error<? endif; ?>">
-            <label class="control-label" for="article">Text Body </label>
+        <div class="form-group <? if (form_error('article') != null): ?>error<? endif; ?>">
+            <label for="article">Text Body</label>
             <div class="controls">
-                <textarea class="span4" name="article" rows="8" cols="15" maxlength=5000><?= set_value('article'); ?></textarea>
+                <textarea class="form-control span4" name="article" rows="8" cols="15" maxlength=5000><?= set_value('article'); ?></textarea>
                 <? if (form_error('article') != null): ?><span class="help-inline"><?= form_error('article') ?></span><? endif; ?>
             </div>
         </div>
