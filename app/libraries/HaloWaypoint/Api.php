cf492dd480773326586e106cc5cfdff10140434f
Connor Tumbleson <connor.tumbleson@gmail.com>
2014-01-25 19:53:03 -0600

    more boilerplate of Api class

--- app/libraries/HaloWaypoint/Api.php
+++ app/libraries/HaloWaypoint/Api.php
@@ -1,12 +1,59 @@
 <?php
 
-namespace HaloWaypoint\Api;
+namespace HaloWaypoint;
 
 class Api
 {
+	private $curl;
 
-	private function getSpartanAuthKey()
+	private $url = "@todo put url";
+
+	function __construct() {
+		$this->curl = new \Curl;
+	}
+
+	/**
+	 * @return array
+	 */
+	public static function getChallenges()
 	{
 
 	}
+
+	/**
+	 * @return string
+	 */
+	private function getSpartanAuthKey()
+	{
+		if (Cache::has('SpartanAuthKey'))
+		{
+			return Cache::get('SpartanAuthKey')->SpartanToken;
+		}
+		else
+		{
+			return "@todo get spartan key";
+		}
+	}
+
+	private function setHeaders($auth = false)
+	{
+		if ($auth)
+		{
+			$key = $this->getSpartanAuthKey();
+
+			$header_array = [
+				'Accept: application/json',
+				'X-343-Authorization-Spartan: ' . $key
+			];
+		}
+		else
+		{
+			$header_array = [
+				'Accept: application/json'
+			];
+		}
+
+		$this->curl->option('HTTPHEADER', $header_array);
+	}
+
 }
