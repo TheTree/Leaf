6eb9632b4f8d9532e09c78a8b239897af616fd5a
Connor Tumbleson <connor.tumbleson@gmail.com>
2014-01-26 13:36:55 -0600

    added utils/helpers for api work

---
+++ app/libraries/HaloWaypoint/Utils.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace HaloWaypoint;
+
+use Library\Helpers;
+
+class Utils
+{
+	public static function getBadgeColor($id)
+	{
+		switch($id)
+		{
+			case 0: return 'warning';
+			case 1: return 'info';
+			case 2: return 'primary';
+			default: return '';
+		}
+	}
+
+	/**
+	 * @param $challenges
+	 * @return array
+	 */
+	public static function prettifyChallenges($challenges)
+	{
+		foreach($challenges as $key => $challenge)
+		{
+			$challenges[$key]->Badge = Utils::getBadgeColor($challenge->CategoryId);
+			$challenges[$key]->XpReward = number_format($challenge->XpReward);
+			$challenges[$key]->EndDate = Helpers::time_duration(strtotime($challenge->EndDate, true) - time());
+		}
+
+		return $challenges;
+	}
+}
