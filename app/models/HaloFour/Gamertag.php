adc6d2e014713dc4ee2d1bd0c673ad7aca778bd4
Connor Tumbleson <connor.tumbleson@gmail.com>
2014-01-25 18:16:46 -0600

    added Gamertag model.

---
+++ app/models/HaloFour/Gamertag.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace HaloFour;
+
+use Jenssegers\Mongodb\Model as Eloquent;
+
+class Gamertag extends Eloquent {
+
+	protected $collection = "h4_gamertags";
+
+	public $timestamps = false;
+
+	protected $guarded = ['0x00', '0x15', '0x16', '0x18', '0x2a', '0x3d', '0x3e', '0x3f', '0x40', '0x41'];
+
+
+	public function get0x1dAttribute($value)
+	{
+		return self::unpack_msg($value);
+	}
+
+	public function get0x25Attribute($value)
+	{
+		return self::unpack_msg($value);
+	}
+
+	public function get0x27Attribute($value)
+	{
+		return self::unpack_msg($value);
+	}
+
+	/**
+	 * Decodes utf8 strings from MongoDB and unpacks back into an array from msgpack
+	 *
+	 * @param $value
+	 * @return mixed
+	 */
+	private function unpack_msg($value)
+	{
+		return msgpack_unpack(utf8_decode($value));
+	}
+}
